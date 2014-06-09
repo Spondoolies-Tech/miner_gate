df43c1e9f66e1b2059640ddf83211183261cf120
zvisha <zvisha@gmail.com>
2014-06-09 03:03:14 +0300

    1.4.0

--- miner_gate/hammer_lib.c
+++ miner_gate/hammer_lib.c
@@ -789,7 +789,7 @@ void one_minute_tasks() {
   // Give them chance to raise over 3 hours if system got colder
   psyslog("Last minute rate: %d (m:%d, nm:%d)\n", (vm.solved_difficulty_total*4/60), vm.mining_time, vm.not_mining_time)
   vm.solved_difficulty_total = 0;
-  if (cnt%(60*24) == 0) {
+  if (cnt%(60*6) == 0) {
     // Lets forget all scaling once every day!
     for (int i = 0 ; i < LOOP_COUNT; i++) {
       if (vm.loop[i].enabled_loop) {
